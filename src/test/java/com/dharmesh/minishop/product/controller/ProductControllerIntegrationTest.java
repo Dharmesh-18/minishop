983ab706f481108766c70965114dd606b1d53f5f
Dharmesh <dharmeshtiwari1280@gmail.com>
2026-08-08 07:51:30 +0530

    Add unit and integration tests

---
+++ src/test/java/com/dharmesh/minishop/product/controller/ProductControllerIntegrationTest.java
@@ -0,0 +1,4 @@
+package com.dharmesh.minishop.product.controller;
+
+public class ProductControllerIntegrationTest {
+}
---
+++ src/test/java/com/dharmesh/minishop/product/service/ProductServiceTest.java
@@ -0,0 +1,4 @@
+package com.dharmesh.minishop.product.service;
+
+public class ProductServiceTest {
+}
